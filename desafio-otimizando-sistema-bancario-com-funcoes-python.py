87a65f92be2de7d655930a9713b595c226665a3c
Igor Pamplona <igortrindade97@hotmail.com>
2024-10-20 20:02:21 -0300

    Create desafio-otimizando-sistema-bancario-com-funcoes-python.py

---
+++ desafio-otimizando-sistema-bancario-com-funcoes-python.py
@@ -0,0 +1,69 @@
+# Variáveis globais
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+def depositar():
+    global saldo, extrato
+    valor = float(input("Informe o valor do depósito: "))
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+        print("Depósito realizado com sucesso!")
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def sacar():
+    global saldo, extrato, numero_saques
+    valor = float(input("Informe o valor do saque: "))
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+        print("Saque realizado com sucesso!")
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def exibir_extrato():
+    global saldo, extrato
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+def menu():
+    menu_texto = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+    while True:
+        opcao = input(menu_texto)
+        if opcao == "d":
+            depositar()
+        elif opcao == "s":
+            sacar()
+        elif opcao == "e":
+            exibir_extrato()
+        elif opcao == "q":
+            print("Obrigado por utilizar o sistema bancário!")
+            break
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+if __name__ == "__main__":
+    menu()
